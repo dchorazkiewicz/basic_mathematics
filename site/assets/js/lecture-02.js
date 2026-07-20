6144b21f27c3f5962b603af8d0d3641733c1cc46
Damian Chorążkiewicz <damian.ch.82@gmail.com>
2026-07-20 14:58:22 +0200

    Load point arithmetic figure in lecture 2

--- site/assets/js/lecture-02.js
+++ site/assets/js/lecture-02.js
@@ -34,6 +34,7 @@
 
     await loadScript('../assets/js/fullscreen.js');
     await loadScript('../assets/js/jsx-board-layout.js');
+    await loadScript('../assets/point-arithmetic.js');
     await loadScript('../assets/vector-equivalence.js');
     await loadScript('../assets/vector-addition.js');
     await loadScript('../assets/vector-scaling.js');
